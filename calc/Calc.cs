8abcf0ad4c9eaf534a7f01ff98931ce5907c584d
Northern Light <33227564+N0rthernL1ght@users.noreply.github.com>
2018-08-12 23:47:25 +0700

    Update Calc.cs

--- calc/Calc.cs
+++ calc/Calc.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//класс калькулятора с реализацией
 namespace calc
 {
     class Calc : InterfaceCalc1
